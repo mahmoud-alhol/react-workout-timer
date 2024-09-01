bf542ac050782298adcac90ce579b0c3e4da1480
mahmoud-alhol <mahmoud.asem.w@gmail.com>
2024-09-01 20:00:00 +0300

    optimized code using memoization

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,5 +1,4 @@
-import { useState } from 'react';
-import clickSound from './ClickSound.m4a';
+import { memo, useState } from "react";
 
 function Calculator({ workouts, allowSound }) {
   const [number, setNumber] = useState(workouts.at(0).numExercises);
@@ -10,12 +9,6 @@ function Calculator({ workouts, allowSound }) {
   const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
   const mins = Math.floor(duration);
   const seconds = (duration - mins) * 60;
-
-  const playSound = function () {
-    if (!allowSound) return;
-    const sound = new Audio(clickSound);
-    sound.play();
-  };
 
   return (
     <>
@@ -33,9 +26,9 @@ function Calculator({ workouts, allowSound }) {
         <div>
           <label>How many sets?</label>
           <input
-            type='range'
-            min='1'
-            max='5'
+            type="range"
+            min="1"
+            max="5"
             value={sets}
             onChange={(e) => setSets(e.target.value)}
           />
@@ -44,10 +37,10 @@ function Calculator({ workouts, allowSound }) {
         <div>
           <label>How fast are you?</label>
           <input
-            type='range'
-            min='30'
-            max='180'
-            step='30'
+            type="range"
+            min="30"
+            max="180"
+            step="30"
             value={speed}
             onChange={(e) => setSpeed(e.target.value)}
           />
@@ -56,9 +49,9 @@ function Calculator({ workouts, allowSound }) {
         <div>
           <label>Break length</label>
           <input
-            type='range'
-            min='1'
-            max='10'
+            type="range"
+            min="1"
+            max="10"
             value={durationBreak}
             onChange={(e) => setDurationBreak(e.target.value)}
           />
@@ -68,8 +61,8 @@ function Calculator({ workouts, allowSound }) {
       <section>
         <button onClick={() => {}}>–</button>
         <p>
-          {mins < 10 && '0'}
-          {mins}:{seconds < 10 && '0'}
+          {mins < 10 && "0"}
+          {mins}:{seconds < 10 && "0"}
           {seconds}
         </p>
         <button onClick={() => {}}>+</button>
@@ -78,4 +71,4 @@ function Calculator({ workouts, allowSound }) {
   );
 }
 
-export default Calculator;
+export default memo(Calculator);
